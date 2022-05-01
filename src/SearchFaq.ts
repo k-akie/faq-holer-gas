227bf913486b1ded69af0b297d54487109ceb9f8
k-akie <kikuchiakie7@gmail.com>
2022-05-01 16:37:26 +0900

    形態素解析を改良

--- src/SearchFaq.ts
+++ src/SearchFaq.ts
@@ -1,18 +1,74 @@
 // main#searchFaq の処理を分割したもの
 
 export class SearchFaq {
-  /** 質問文からキーワードを抽出する */
-  static textAnalyze(text: string): string[] {
-    // 簡易形態素解析
+  /** 簡易形態素解析 */
+  static _textAnalyze(text: string): string[] {
     // https://takuya-1st.hatenablog.jp/entry/2016/04/02/145017
-    const r=/[一-龠]+|[ぁ-ん]+|[ァ-ヴー]+|[a-zA-Z0-9\-]+|[ａ-ｚＡ-Ｚ０-９]+/g;
+    const r = /[一-龠]+|[ぁ-ん]+|[ァ-ヴー]+|[a-zA-Z0-9\-]+|[ａ-ｚＡ-Ｚ０-９]+/g;
     const keywordArray = text.match(r);
     if (keywordArray == null) return []; // FIXME エラーハンドリングする
     return keywordArray;
   }
 
+  /** 簡易形態素解析(助詞考慮) */
+  static textAnalyze(text: string, onlyNoun: boolean = true): string[] {
+    const keywordArray = this._textAnalyze(text);
+    // https://qiita.com/kinoshita_yuri/items/e15f143981f1616994ed
+    const result: string[] = [];
+    let kanji = false;
+    for (const keyword of keywordArray) {
+      if (keyword.match(/[ぁ-んー]/g)) {
+        // 先頭にあったら助詞とみなす
+        const particlePre = /^[はがのにへともでを]/g;
+        const mathcedPre = keyword.match(particlePre);
+        if (mathcedPre) {
+          if (!onlyNoun) {
+            result.push(mathcedPre[0]);
+          }
+          if (keyword.length > 1) {
+            result.push(keyword.replace(particlePre, ''));
+          }
+          kanji = false;
+          continue;
+        }
+
+        // 末尾にあったら助詞とみなす
+        const particleSuf = /(.+)(ので)|(から)|(まで)|[もはがでを]$/g;
+        const matchedSuf = keyword.match(particleSuf);
+        if (matchedSuf) {
+          result.push(keyword.replace(particleSuf, ''));
+          if (!onlyNoun) {
+            result.push(matchedSuf[0]);
+          }
+          kanji = false;
+          continue;
+        }
+      }
+
+      // 助詞じゃないのに漢字のあとにひらがな1文字があるときは、送り仮名とみなす
+      if (kanji && keyword.length == 1) {
+        const preItem = result[result.length - 1];
+        result[result.length - 1] = preItem + keyword;
+        kanji = false;
+        continue;
+      }
+
+      kanji = false;
+      if (keyword.match(/[一-龠]+/g)) {
+        kanji = true;
+      }
+
+      result.push(keyword);
+    }
+
+    return result;
+  }
+
   /** キーワード配列に合致するデータを探す */
-  static serachByKeyword(keywordArray: string[], targetData: string[][]): string[][] {
+  static serachByKeyword(
+      keywordArray: string[],
+      targetData: string[][],
+  ): string[][] {
     const results: string[][] = [];
     for (const keyword of keywordArray) {
       const datas = targetData.filter((data) => data[0] == keyword);
--- test/SearchFaq.test.ts
+++ test/SearchFaq.test.ts
@@ -1,17 +1,51 @@
 import {SearchFaq} from '../src/SearchFaq';
 
 
-describe('textAnalyze', () => {
+describe('_textAnalyze', () => {
   test('分割されないとき', () => {
-    expect(SearchFaq.textAnalyze('')).toStrictEqual([]);
-    expect(SearchFaq.textAnalyze(',')).toStrictEqual([]);
-    expect(SearchFaq.textAnalyze('あいう')).toStrictEqual(['あいう']);
+    expect(SearchFaq._textAnalyze('')).toStrictEqual([]);
+    expect(SearchFaq._textAnalyze(',')).toStrictEqual([]);
+    expect(SearchFaq._textAnalyze('あいう')).toStrictEqual(['あいう']);
   });
   test('分割されるとき', () => {
-    expect(SearchFaq.textAnalyze('足し算')).toStrictEqual(['足', 'し', '算']);
-    expect(SearchFaq.textAnalyze('こどもの日')).toStrictEqual(['こどもの', '日']);
-    expect(SearchFaq.textAnalyze('システム開発')).toStrictEqual(['システム', '開発']);
-    expect(SearchFaq.textAnalyze('KPTについて')).toStrictEqual(['KPT', 'について']);
+    expect(SearchFaq._textAnalyze('足し算')).toStrictEqual(['足', 'し', '算']);
+    expect(SearchFaq._textAnalyze('こどもの日')).toStrictEqual(['こどもの', '日']);
+    expect(SearchFaq._textAnalyze('システム開発')).toStrictEqual(['システム', '開発']);
+    expect(SearchFaq._textAnalyze('KPTについて')).toStrictEqual(['KPT', 'について']);
+  });
+});
+
+describe('textAnalyze: 助詞を含んで抽出', () => {
+  test.each([
+    ['吾輩は猫である。', ['吾輩', 'は', '猫', 'で', 'ある']],
+    ['名前はまだ無い。', ['名前', 'は', 'まだ', '無い']],
+    ['最寄り駅はどこですか', ['最寄り', '駅', 'は', 'どこですか']],
+    ['どんなスキルが必要なのか', ['どんな', 'スキル', 'が', '必要', 'なのか']],
+  ])('うまく解析できるとき(%s)', (input, expected) => {
+    expect(SearchFaq.textAnalyze(input, false)).toStrictEqual(expected);
+  });
+  test.each([
+    ['タバコは吸えますか', ['タバコ', 'は', '吸', 'えますか']],
+    ['持ち帰りはできますか', ['持ち', '帰', 'りはできますか']],
+  ])('かなが続くとき(%s)', (input, expected) => {
+    expect(SearchFaq.textAnalyze(input, false)).toStrictEqual(expected);
+  });
+});
+
+describe('textAnalyze: 助詞を除いて抽出', () => {
+  test.each([
+    ['吾輩は猫である。', ['吾輩', '猫', 'ある']],
+    ['名前はまだ無い。', ['名前', 'まだ', '無い']],
+    ['最寄り駅はどこですか', ['最寄り', '駅', 'どこですか']],
+    ['どんなスキルが必要なのか', ['どんな', 'スキル', '必要', 'なのか']],
+  ])('うまく解析できるとき(%s)', (input, expected) => {
+    expect(SearchFaq.textAnalyze(input, true)).toStrictEqual(expected);
+  });
+  test.each([
+    ['タバコは吸えますか', ['タバコ', '吸', 'えますか']],
+    ['持ち帰りはできますか', ['持ち', '帰', 'りはできますか']],
+  ])('かなが続くとき(%s)', (input, expected) => {
+    expect(SearchFaq.textAnalyze(input, true)).toStrictEqual(expected);
   });
 });
 
